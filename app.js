0fcb7752356135a3a48be45e544c815404dd5ea0
Justin179 <Justin179>
2017-01-23 10:11:23 +0800

    starting point(comments added)

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 3000;
-
+// ejs view
 app.set('view engine', 'ejs');
+
+// static resource(empty files)
 app.use('/assets', express.static(__dirname + '/public'));
 
+// localhost:3000
 app.get('/', function(req, res) {
 	
-	res.render('index');
+	res.render('index'); // 會自動找views目錄下的index
 	
 });
 
+// listen to port 3000
+var port = process.env.PORT || 3000;
 app.listen(port);
